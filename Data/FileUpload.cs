a0a4231b7a6a5697fe4bea8cb3bc817b22dddc38
sefatanam <sefatanam@gmail.com>
2020-01-02 19:24:11 +0600

    file upload

---
+++ Data/FileUpload.cs
@@ -0,0 +1,32 @@
+﻿using AweBlazor.Service;
+using BlazorInputFile;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AweBlazor.Data
+{
+    public class FileUpload : IFileUpload
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FileUpload(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task UploadAsync(IFileListEntry fileEntry)
+        {
+            var path = Path.Combine(_webHostEnvironment.ContentRootPath, "Upload", fileEntry.Name);
+            var memoryStream = new MemoryStream();
+            await fileEntry.Data.CopyToAsync(memoryStream);
+            using (FileStream fileStream = new FileStream(path, FileMode.Create, FileAccess.Write))
+            {
+                memoryStream.WriteTo(fileStream);
+            }
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using AweBlazor.Data;
+using AweBlazor.Service;
 
 namespace AweBlazor
 {
@@ -28,7 +29,10 @@ namespace AweBlazor
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            //This for student Data Grid
             services.AddScoped<StudentService>();
+            //This for file upload
+            services.AddScoped<IFileUpload, FileUpload>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
